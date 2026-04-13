fd5bed8c1dd020929a19b81a0a5575cd7cdf01f3
Juan Esteban <102327436+JuanesVVV@users.noreply.github.com>
2026-04-13 00:17:32 +0000

    API y logo

--- AppReactyExpo/App.js
+++ AppReactyExpo/App.js
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
 
+const API_CONFIG = {
+    BASE_URL: 'https://gamebrain.co/api',
+    API_KEY: '2813d4a7f5b647a2932d40f132c48e71'
+};
+
+export async function callGameBrain(endpoint, params = {}) {
+    const queryParams = new URLSearchParams({
+        ...params,
+        'api-key': API_CONFIG.API_KEY
+    });
+
+    const response = await fetch(`${API_CONFIG.BASE_URL}${endpoint}?${queryParams}`);
+    return await response.json();
+}
+
 const Inicio = () => (
   <View style={styles.screen}>
     <Text>Inicio</Text>
